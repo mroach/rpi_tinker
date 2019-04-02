b5b27a576bd11d57e74324ca3893d6df10dbc73e
mroach <git@c.mroach.com>
2019-04-02 06:48:19 +0100

    Need circuits_spi 0.1.3 fixes

--- mix.exs
+++ mix.exs
@@ -23,8 +23,8 @@ defmodule Tinker.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:circuits_gpio, "~> 0.4"},
-      {:circuits_spi, "~> 0.1"},
+      {:circuits_gpio, "~> 0.4.1"},
+      {:circuits_spi, "~> 0.1.3"},
       {:nerves_io_rc522, "~> 0.1.0"}
     ]
   end
